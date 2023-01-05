f75e892d72ae03a21936858c29d1833187068c89
bucknorris336 <buckblocker90gmail.com>
2023-01-05 18:34:08 -0500

    added html and js

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+var APIKey = "0c6426e6aac7f0e8adddb2e92d819eb4";
+var city = "Durham";
+
+var queryURL =
+  `http://api.openweathermap.org/data/2.5/weather?q=` +
+  city +
+  "&appid=" +
+  APIKey +
+  "&units=imperial";
+
+fetch(queryURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    getFiveDay(data.coord.lat, data.coord.lon);
+    console.log(data);
+  });
+function getFiveDay(lat, lon) {
+  var queryURL2 =
+    "https://api.openweathermap.org/data/3.0/onecall?lat=" +
+    lat +
+    "&lon=" +
+    lon +
+    "&appid=" +
+    APIKey;
+  fetch(queryURL2)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+}
+
+//https://api.openweathermap.org/data/2.5/weather?id={city id}&appid={API key}
