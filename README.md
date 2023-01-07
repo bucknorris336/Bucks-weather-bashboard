4166127f570237d6e814aa3d0ed7f4254089a812
bucknorris336 <buckblocker90gmail.com>
2023-01-06 20:31:30 -0500

    added js

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Bucks-weather-bashboard
 
-A Weather Dashboard
+#A Weather Dashboard
 
 GIVEN a weather dashboard with form inputs
 WHEN I search for a city
@@ -11,3 +11,5 @@ WHEN I view future weather conditions for that city
 THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
 WHEN I click on a city in the search history
 THEN I am again presented with current and future conditions for that city
+
+An app that allows you to find the current weather in whatever city you'd like. It will even show the forecast for the next five days. Along with the temp, you can see ____, _____, and _____. If you want to search for more cities, you're in luck, search as many as you want. It will keep the previous city searches beneathe the searh bar so if you want to, you may click back on it.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,77 @@
-var APIKey = "0c6426e6aac7f0e8adddb2e92d819eb4";
-var city = "Durham";
+$(function () {
+  var APIKey = "0c6426e6aac7f0e8adddb2e92d819eb4";
+  function createCard(date, temp, humidity, wind, index) {
+    if (i != 0) {
+      var card = `       
+       <div class="card full-height">
+        <h5 class="card-title">${date}</h5>
+         <p class="card-text">temp: ${temp}</p>
+         <p class="card-text">humidity: ${humidity}</p>
+         <p class="card-text">wind: ${wind}</p>
+        </div>`;
 
-var queryURL =
-  `http://api.openweathermap.org/data/2.5/weather?q=` +
-  city +
-  "&appid=" +
-  APIKey +
-  "&units=imperial";
+      $("#forecast").html(card);
+    } else {
+      var todaysWeather = `       <div class="todays-weather">
+      <h2>${date}.</h2>
+     <p>temp: ${temp}</p>
+     <p>humidity: ${humidity}</p>
+     <p>wind: ${wind}</p>
+     </div>`;
+      $("#forecast").html(todaysWeather);
+    }
+  }
 
-fetch(queryURL)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    getFiveDay(data.coord.lat, data.coord.lon);
-    console.log(data);
+  function mapData(weatherArray) {
+    for (var i = 0; i < weatherArray.length; i++) {
+      var day = weatherArray[i];
+      var date = day.name + " (" + new Date().toLocaleDateString();
+      var temp = day.main.temp;
+      var humidity = day.main.humidity;
+      var windSpeed = day.wind.speed;
+      createCard(date, temp, humidity, windSpeed, i);
+      // console.log(day);
+    }
+  }
+
+  function getFiveDay(lat, lon) {
+    var queryURL2 =
+      "https://api.openweathermap.org/day/2.5/forecast?lat=" +
+      lat +
+      "&lon=" +
+      lon +
+      "&cnt=6&appid=" +
+      APIKey;
+    fetch(queryURL2)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        mapData(data.list);
+      });
+  }
+
+  function handleSubmit(value) {
+    console.log("value=", value);
+    var queryURL =
+      `http://api.openweathermap.org/data/2.5/weather?q=` +
+      value +
+      "&appid=" +
+      APIKey +
+      "&units=imperial";
+    fetch(queryURL)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        getFiveDay(data.coord.lat, data.coord.lon);
+      });
+  }
+
+  $("#submit-btn").on("click", function (event) {
+    event.preventDefault();
+    var inputValue = event.target.form[0].value;
+    handleSubmit(inputValue);
   });
-function getFiveDay(lat, lon) {
-  var queryURL2 =
-    "https://api.openweathermap.org/data/2.5/forecast?lat=" +
-    lat +
-    "&lon=" +
-    lon +
-    "&appid=" +
-    APIKey;
-  fetch(queryURL2)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-    });
-}
-
-//https://api.openweathermap.org/data/2.5/weather?id={city id}&appid={API key}
+  //https://api.openweathermap.org/data/2.5/weather?id={city id}&appid={API key}
+});
