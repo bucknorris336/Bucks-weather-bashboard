2b9140722441f83537fa1c5acdd2b1c4433fc8d2
bucknorris336 <buckblocker90gmail.com>
2023-01-12 14:51:44 -0500

    added changes to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,88 +26,92 @@ function fetchCoords(searchCity) {
       console.log(data);
       fetchWeather(data.coord);
     });
-
-  function fetchWeather(location) {
-    var { lat } = location;
-    var { lon } = location;
-    var city = location.name;
-
-    var queryURL2 =
-      "https://api.openweathermap.org/data/2.5/forecast?lat=" +
-      lat +
-      "&lon=" +
-      lon +
-      "&cnt=6&appid=" +
-      APIKey;
-    fetch(queryURL2)
-      .then(function (res) {
-        return res.json();
-      })
-      .then(function (data) {
-        console.log("newData", data);
-        renderItems(city, data);
-      })
-      .catch(function (err) {
-        console.error(err);
-      });
-  }
-function renderItems(city, data) {
-  renderCurrentWeather(city, data.list[0], data.city.timezone);
-  // renderForecast(data.list);
-}
-  function renderCurrentWeather(city, weather) {
-  var date = dayjs().format('M/D/YYYY');
-  // Store response data from our fetch request in variables
-  var tempF = weather.main.temp;
-  var windMph = weather.wind.speed;
-  var humidity = weather.main.humidity;
-  var iconUrl = `https://openweathermap.org/img/w/${weather.weather[0].icon}.png`;
-  var iconDescription = weather.weather[0].description || weather[0].main;
-
-  var card = document.createElement('div');
-  var cardBody = document.createElement('div');
-  var heading = document.createElement('h2');
-  var weatherIcon = document.createElement('img');
-  var tempEl = document.createElement('p');
-  var windEl = document.createElement('p');
-  var humidityEl = document.createElement('p');
-  function renderCurrentWeather(city, weather) {
-  var date = dayjs().format('M/D/YYYY');
-  // Store response data from our fetch request in variables
-  var tempF = weather.main.temp;
-  var windMph = weather.wind.speed;
-  var humidity = weather.main.humidity;
-  var iconUrl = `https://openweathermap.org/img/w/${weather.weather[0].icon}.png`;
-  var iconDescription = weather.weather[0].description || weather[0].main;
-
-  var card = document.createElement('div');
-  var cardBody = document.createElement('div');
-  var heading = document.createElement('h2');
-  var weatherIcon = document.createElement('img');
-  var tempEl = document.createElement('p');
-  var windEl = document.createElement('p');
-  var humidityEl = document.createElement('p');
-  // var APIUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${searchCity}&lmit=5&appid=${APIKey}`;
-  // fetch(APIUrl)
-  //   .then(function (res) {
-  //     return res.json();
-  //   })
-  //   .then(function (data) {
-  //     if (!data[0]) {
-  //       alert("Location not found");
-  //     } else {
-  //       //appendToHistory(search);
-  //       fetchWeather(data[0]);
-  //       console.log(data[0]);
-  //     }
-  //   })
-  //   .catch(function (err) {
-  //     console.error(err);
-  //   });
 }
 function fetchWeather(location) {
-  var { lat, lon } = location;
-  console.log("data", lat, lon, location);
+  var { lat } = location;
+  var { lon } = location;
+  var city = location.name;
+
+  var queryURL2 =
+    "https://api.openweathermap.org/data/2.5/forecast?units=imperial&lat=" +
+    lat +
+    "&lon=" +
+    lon +
+    "&appid=" +
+    APIKey;
+  fetch(queryURL2)
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      console.log("newData", data);
+      renderCurrentWeather(data);
+    })
+    .catch(function (err) {
+      console.error(err);
+    });
 }
+
+function renderCurrentWeather(data) {
+  var weather = data.list[0];
+  var date = dayjs().format("M/D/YYYY");
+  // Store response data from our fetch request in variables
+  var tempF = weather.main.temp;
+  var windMph = weather.wind.speed;
+  var humidity = weather.main.humidity;
+  var iconUrl = `https://openweathermap.org/img/w/${weather.weather[0].icon}.png`;
+  var iconDescription = weather.weather[0].description || weather[0].main;
+
+  var card = document.createElement("div");
+  var cardBody = document.createElement("div");
+  var heading = document.createElement("h2");
+  var weatherIcon = document.createElement("img");
+  var tempEl = document.createElement("p");
+  tempEl.innerHTML = tempF;
+  var windEl = document.createElement("p");
+  var humidityEl = document.createElement("p");
+
+  var weatherContainer = document.getElementById("weather-container");
+  weatherContainer.append(tempEl);
+
+  // function renderCurrentWeather(city, weather) {
+  //   var date = dayjs().format("M/D/YYYY");
+  //   // Store response data from our fetch request in variables
+  //   var tempF = weather.main.temp;
+  //   var windMph = weather.wind.speed;
+  //   var humidity = weather.main.humidity;
+  //   var iconUrl = `https://openweathermap.org/img/w/${weather.weather[0].icon}.png`;
+  //   var iconDescription = weather.weather[0].description || weather[0].main;
+
+  //   var card = document.createElement("div");
+  //   var cardBody = document.createElement("div");
+  //   var heading = document.createElement("h2");
+  //   var weatherIcon = document.createElement("img");
+  //   var tempEl = document.createElement("p");
+  //   var windEl = document.createElement("p");
+  //   var humidityEl = document.createElement("p");
+}
+// var APIUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${searchCity}&lmit=5&appid=${APIKey}`;
+// fetch(APIUrl)
+//   .then(function (res) {
+//     return res.json();
+//   })
+//   .then(function (data) {
+//     if (!data[0]) {
+//       alert("Location not found");
+//     } else {
+//       //appendToHistory(search);
+//       fetchWeather(data[0]);
+//       console.log(data[0]);
+//     }
+//   })
+//   .catch(function (err) {
+//     console.error(err);
+//   });
+// }
+// function fetchWeather(location) {
+//   var { lat, lon } = location;
+//   console.log("data", lat, lon, location);
+// }
 
 searchForm.addEventListener("submit", handleSearchFormSubmit);
